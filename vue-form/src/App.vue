69fecc633de054989ece069d931fc85b5b29cd80
Sannim <inegg.apps@gmail.com>
2020-08-22 18:15:56 +0900

    로그인 버튼을 눌렀을 때 발생하는 이벤트를 잡아 처리하기

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
-    <form>
+    <form v-on:submit="submitForm">
       <div>
         <label for="username">ID:</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" v-model="username" />
       </div>
       <div>
         <label for="password">PW:</label>
-        <input id="password" type="text" />
+        <input id="password" type="password" v-model="password" />
       </div>
-      <button>로그인</button>
+      <button type="submit">로그인</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      //React의 State
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    //ES6+ 객체를 생성할 때 향상된 문법
+    submitForm(event) {
+      event.preventDefault();
+      console.log("submitted");
+    },
+  },
+};
 </script>
 
 <style>
