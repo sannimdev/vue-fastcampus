1d7016382351ed599f406f9dd2f3e9157e553cba
Sannim <inegg.apps@gmail.com>
2020-08-22 18:09:22 +0900

    로그인 폼 만들기 (모양만)

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <div>
+        <label for="username">ID:</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input id="password" type="text" />
+      </div>
+      <button>로그인</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
